8d2a5004a920e0e1596f10aca8c1d08678298aff
hergarp <78268365+hergarp@users.noreply.github.com>
2021-05-24 23:05:16 +0200

    alap_oldal

---
+++ recept.js
@@ -0,0 +1,81 @@
+var receptTomb = [];
+
+$(function () {
+    $("article").on("click", "tr", megjelenit);
+    $.ajax(
+            {url: "receptek.json",
+                success: function (result) {
+                    receptTomb = (result);
+                    console.log(receptTomb);
+//                  $("article").append(receptTomb+"");
+                    tablazatbaKiir();
+                }
+            });
+
+});
+function megjelenitRecept(recept) {
+    $("section").empty();
+    //adott recept adatainak megjelenitese
+    //Section h2-be a Recept neve
+    $("section").append("<h2></h2>");
+    $("section h2").append(recept.Név);
+    //Section P-be a leírása
+    $("section").append("<p></p>");
+    $("section p").append(recept.Leírás);
+    //Section Divbe a hozzávalók
+    $("section").append("<div></div>");
+////    $("section div").append(recept.img src="Elérési_út");
+//    $("section").append("<img></img>");
+    $("section div #kep").append("<img />").attr(recept.Elérési_út);
+//        var img = $('<img />').attr({
+//            'id': 'myImage',
+//            'src': 'kepek/kukoricaleves.jpg',
+//            'alt': 'JSFiddle logo',
+//            'title': 'JSFiddle logo',
+//            'width': 250
+//        }).appendTo('#kep');
+
+
+    //Sectoin imagebe a képet
+
+}
+function megjelenit() {
+    var ID = Number($(this).attr("id"));
+    console.log(ID);
+    megjelenitRecept(receptTomb[ID - 1]);
+}
+function tablazatbaKiir() {
+    csakFejlec();
+    for (var i = 0; i < receptTomb.length; i++) {
+        $("table").append("<tr id='" + (i + 1) + "'></tr>");
+        for (var item in receptTomb[i]) {
+            if (item === "Hozzávalók") {
+                for (var item2 in receptTomb[i][item]) {
+                    // console.log(receptTomb[i][item]);
+                    for (var item3 in receptTomb[i][item][item2]) {
+                        $("table tr").eq(i + 1).append("<ul><li>" + item3 + " " + receptTomb[i][item][item2][item3] + " </li></ul>");
+
+                    }
+                }
+            } else {
+                $("table tr").eq(i + 1).append("<td>" + receptTomb[i][item] + "</td>");
+            }
+
+
+        }
+
+    }
+
+}
+;
+function csakFejlec() {
+    $("article").append("<table></table>");
+    $("article table").append("<tr id='0'>\n\
+<th>Név</th>\n\
+<th>Elkészítési idő</th>\n\
+<th>Elérési út</th>\n\
+<th>Leírás</th>\n\
+<th>Hozzávalók</th>\n\
+</tr>");
+
+}
